a2f2f400e020d4917f1e3fe14c1ea8bcc780ce83
Jeffrey Gebhardt <jeff@gebbey.com>
2018-05-08 14:22:06 -0700

    added runbook for single Linux VM

--- cleanupResourceGroup/azureDeploy.ps1
+++ cleanupResourceGroup/azureDeploy.ps1
@@ -1,4 +1,6 @@
 $resourceGroup = "jegebhDevRG"
 $templatePath = "C:\Users\v-jegebh\projects\azure-arm-templates\cleanupResourceGroup\clearRG.template.json"
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -Mode Complete -TemplateFile $templatePath -Force
+$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -Mode Complete -TemplateFile $templatePath -Force
+
+$deployment
